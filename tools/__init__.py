e6653ebe36b0546b298665f8050aff9c751e8548
Denali Schlesinger <89662251+dschlesinger@users.noreply.github.com>
2025-06-20 18:25:08 +0000

    Add closest by atomic

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,9 @@
-from .distance import residue_interaction_by_distance, residue_by_id
+from .distance import residue_interaction_by_ca, residue_closest_atomic_interaction, residue_by_id
 from .AminoAcids import AminoAcids3
 from .ViewTools import ViewHandler
 
 ### Create pseudonyms for function
 
 # residue_interaction_by_distance
-ridb = residue_interaction_by_distance
-by_residue_distance =  residue_interaction_by_distance
+ribc = by_residue_ca =  residue_interaction_by_ca
+rcai = by_residue_closest = residue_closest_atomic_interaction
--- tools/distance.py
+++ tools/distance.py
@@ -1,16 +1,32 @@
 import numpy as np
 from Bio import PDB
 
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Any
 from dataclasses import dataclass
 
 import json
 
 @dataclass
+class AtomicID:
+    """Identifier for molecules in the protien"""
+    chain: str
+
+    residue_number: int
+    residue_type: str
+
+    atom_type: str
+    atom_number: int
+
+@dataclass
 class Interaction:
 
   id: str
-  distance: float
+  global_distance: float
+
+  # If using by closest atomic interaction
+  closest_atomic_diffence: float | None = None
+  self_closest: AtomicID | None = None
+  other_closest: AtomicID | None = None
 
 @dataclass
 class InteractingResidue:
@@ -28,6 +44,16 @@ class InteractingResidue:
 
   @classmethod
   def from_json(cls, instance: Dict):
+
+      for interaction in instance['interactions']:
+          
+        if interaction['self_closest']:
+
+            interaction['self_closest'] = AtomicID(**interaction['self_closest'])
+        
+        if interaction['other_closest']:
+
+            interaction['other_closest'] = AtomicID(**interaction['other_closest'])
       
       interactions = [Interaction(**i) for i in instance['interactions']]
 
@@ -40,6 +66,16 @@ class InteractingResidue:
     base_json = self.__dict__
 
     # __dict__ interactions
+    for interaction in base_json['interactions']:
+
+        if interaction.self_closest:
+
+            interaction.self_closest = interaction.self_closest.__dict__
+
+        if interaction.other_closest:
+
+            interaction.other_closest = interaction.other_closest.__dict__
+
     base_json['interactions'] = [i.__dict__ for i in base_json['interactions']]
 
     return base_json
@@ -53,8 +89,81 @@ def residue_by_id(interacting_residue: List[InteractingResidue], id: str) -> Int
 
             return ir
 
+def Bio_object_by_residue(a: PDB.Chain.Chain, r: InteractingResidue) -> Any: # idk what type
 
-def residue_interaction_by_distance(a: PDB.Chain.Chain, b: PDB.Chain.Chain, distance: float = 6.0) -> List[InteractingResidue]:
+    # Get residue
+    for res in a.get_residues():
+
+        if r.position == res.id[1]:
+
+            return res
+
+def residue_closest_atomic_interaction(a: PDB.Chain.Chain, b: PDB.Chain.Chain, distance: float = 6.0) -> List[InteractingResidue]:
+
+    by_CA_distance: List[InteractingResidue] = residue_interaction_by_ca(a, b, distance)
+
+    # Fill in None fields
+    for self_residue in by_CA_distance:
+
+        if self_residue.chain == a.id:
+            self_chain = a
+            other_chain = b
+
+        else:
+            self_chain = b
+            other_chain = a
+
+        self_Bio_object = Bio_object_by_residue(self_chain, self_residue)
+
+        self_atom_coord_matrix = np.array([atom.coord for atom in self_Bio_object.get_atoms()])
+
+        for interaction in self_residue.interactions:
+
+            other_residue: InteractingResidue = residue_by_id(by_CA_distance, interaction.id)
+
+            other_Bio_object = Bio_object_by_residue(other_chain, other_residue)
+
+            other_atom_coord_matrix = np.array([atom.coord for atom in other_Bio_object.get_atoms()])
+
+            # Self is rows other is columns
+            diff = self_atom_coord_matrix[:, np.newaxis, :] - other_atom_coord_matrix[np.newaxis, :, :]
+
+            # Uses euclidean distance
+            distance_mat = np.sqrt(np.sum(diff ** 2, axis=2))
+
+            closest_atoms = distance_mat.argmin()
+
+            # Get coords from flat index
+            closest_atoms = np.unravel_index(closest_atoms, distance_mat.shape)
+
+            interaction.closest_atomic_diffence = distance_mat[closest_atoms].item()
+
+            self_atom = list(self_Bio_object.get_atoms())[closest_atoms[0].item()]
+            other_atom = list(other_Bio_object.get_atoms())[closest_atoms[1].item()]
+
+            interaction.self_closest = AtomicID(
+                chain=self_chain.id,
+                residue_number=self_residue.position,
+                residue_type=self_residue.residue,
+                atom_number=closest_atoms[0].item(),
+                atom_type=self_atom.element,
+            )
+
+            interaction.other_closest = AtomicID(
+                chain=other_chain.id,
+                residue_number=other_residue.position,
+                residue_type=other_residue.residue,
+                atom_number=closest_atoms[1].item(),
+                atom_type=other_atom.element,
+            )
+    
+    # bc why not
+    by_atomic_diffrence = by_CA_distance
+
+    return by_atomic_diffrence
+
+
+def residue_interaction_by_ca(a: PDB.Chain.Chain, b: PDB.Chain.Chain, distance: float = 6.0) -> List[InteractingResidue]:
     """Returns a list of all residues with at least one interaction under the distance limit
 
     Args:
@@ -101,8 +210,8 @@ def residue_interaction_by_distance(a: PDB.Chain.Chain, b: PDB.Chain.Chain, dist
       b_key = ':'.join((b.id, str(pos[1])))
 
       # Create Interaction objects
-      a_interaction = Interaction(id=a_key, distance=float(euc_dist))
-      b_interaction = Interaction(id=b_key, distance=float(euc_dist))
+      a_interaction = Interaction(id=a_key, global_distance=float(euc_dist))
+      b_interaction = Interaction(id=b_key, global_distance=float(euc_dist))
 
       # Add to chain a
       if a_key in interacting_residues:
